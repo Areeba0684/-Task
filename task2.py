6cae880892ad703c8929259b845d9930ea466a46
Aminafarooq21 <124820331+Aminafarooq21@users.noreply.github.com>
2023-02-21 20:41:12 +0500

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,10 @@
+#printing first 10 numbers using for loop
+for n in range(1, 11):
+   print(n)
+   
+#printing first 10 numbers using while loop
+
+   i = 1
+while(i<=10):
+    print(i)
+    i += 1
